84a013fc479e1c0b6a07cba475702da24b382b33
Daniele <Iximiel@users.noreply.github.com>
2025-12-02 22:34:41 +0100

    solved 1-2

--- puzzle1/puzzle1.cpp
+++ puzzle1/puzzle1.cpp
@@ -6,6 +6,23 @@
 struct filedeleter {
   void operator()(FILE *f) { std::fclose(f); }
 };
+struct divtie {
+  int &quot;
+  int &rem;
+  divtie(int &q, int &r) : quot(q), rem(r) {}
+  divtie &operator=(const std::div_t &d) {
+    quot = d.quot;
+    rem = d.rem;
+    return *this;
+    // using
+    //```
+    // auto x=div()
+    // q=x.quot
+    // r=x.rem
+    //```
+    // would have taken much less effort xD
+  }
+};
 
 int main() {
   constexpr int dialmax = 99 + 1;
@@ -14,9 +31,11 @@ int main() {
   assert((f.get(), "error in opening file"));
   char dir;
   int step;
-  long pw = 0;
+  long pw1 = 0;
+  long pw2 = 0;
   // I have never used fscanf before
   while (fscanf(f.get(), "%c%i\n", &dir, &step) != EOF) {
+    int clicks = 0;
     switch (dir) {
     case 'L':
       dialval -= step;
@@ -24,17 +43,27 @@ int main() {
     case 'R':
       dialval += step;
     }
-    if (dialval >= dialmax) {
-      dialval %= dialmax;
-    } else {
-      // this is the evil part of the puzzle
-      while (dialval < 0) {
-        dialval += dialmax;
-      }
+
+    while (dialval > dialmax) {
+      ++clicks;
+      dialval -= dialmax;
     }
-    if (dialval == 0) {
-      ++pw;
+    // this is evil
+    if (dialval < 0 && (dialval + step) == 0) {
+      clicks--;
     }
+    while (dialval < 0) {
+      dialval += dialmax;
+      ++clicks;
+    }
+
+    if (dialval == 0 || dialval == dialmax) {
+      dialval = 0;
+      ++clicks;
+      ++pw1;
+    }
+    pw2 += clicks;
   }
-  std::cout << "password: " << pw << "\n";
+  std::cout << "password part1: " << pw1 << "\n";
+  std::cout << "password part2: " << pw2 << "\n";
 }
