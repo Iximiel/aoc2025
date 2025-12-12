08556d9a0f0f4a00d2149b3f31d8c77f6e342b81
Daniele <Iximiel@users.noreply.github.com>
2025-12-12 01:37:59 +0100

    puzzle 9 part 2 is slow

--- puzzle9/puzzle.cpp
+++ puzzle9/puzzle.cpp
@@ -18,16 +18,15 @@ struct filedeleter {
 struct point {
   long x;
   long y;
+  point operator-(const point &other) const {
+    return {x - other.x, y - other.y};
+  }
 };
 std::ostream &operator<<(std::ostream &os, const point &p) {
   os << p.x << " " << p.y;
   return os;
 }
-unsigned long distance(const point &a, const point &b) {
-  unsigned long x = (a.x > b.x) ? a.x - b.x : b.x - a.x;
-  unsigned long y = (a.y > b.y) ? a.y - b.y : b.y - a.y;
-  return x * x + y * y;
-}
+
 unsigned long area(const point &a, const point &b) {
   // the perimeter is in the area
   unsigned long x = 1 + ((a.x > b.x) ? a.x - b.x : b.x - a.x);
@@ -35,7 +34,49 @@ unsigned long area(const point &a, const point &b) {
   return x * y;
 }
 
-// everithing is public because I do not want to setup getters
+bool PIPCheck(const point &a, const point &b,
+              const std::vector<point> &perimeter) {
+  // this assumess that perimeter[0] == perimeter.back()
+  // we are casting a ray from each point to the right
+  point up = {std::max(a.x, b.x), std::max(a.y, b.y)};
+  point low = {std::min(a.x, b.x), std::min(a.y, b.y)};
+  for (long x = low.x; x <= up.x; ++x) {
+    for (long y = low.y; y <= up.y; ++y) {
+      auto prev = perimeter[0];
+      unsigned intersections = 0;
+      for (unsigned i = 1; i < perimeter.size(); ++i) {
+        auto edge = perimeter[i] - prev;
+        // assert(edge.x == 0 || edge.y == 0);
+        if (edge.y == 0 && y == perimeter[i].y) {
+          // the edge is horizontal, the point is inside if it is on the edge
+          if ((edge.x > 0) ? (prev.x < x && x < perimeter[i].x)
+                           : (prev.x > x && x > perimeter[i].x)) {
+            intersections = 1;
+            break;
+          }
+        } else {
+          // the edge is vertical
+          if (x < perimeter[i].x) {
+            if ((edge.y > 0) ? (prev.y < y && y < perimeter[i].y)
+                             : (prev.y > y && y > perimeter[i].y)) {
+              if (x == perimeter[i].x) { // the point is on the edge
+                intersections = 1;
+                break;
+              }
+              ++intersections;
+            }
+          }
+        }
+        prev = perimeter[i];
+      }
+      if (intersections % 2 == 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+//  everithing is public because I do not want to setup getters
 int main() {
 // #define TESTING
 #ifdef TESTING
@@ -68,4 +109,21 @@ int main() {
     }
   }
   std::cout << "biggest area part1: " << maxRectangle << "\n";
+
+  size_t maxRectangleEncased = 0;
+  // needed for the PIP (point in poligon) algorithm
+  redTiles.push_back(redTiles[0]);
+  for (unsigned i = 0; i < redTiles.size() - 1; ++i) {
+    for (unsigned j = i + 1; j < redTiles.size() - 1; ++j) {
+      // the following line is added for the lulz
+      auto A = area(redTiles[i], redTiles[j]);
+      auto isInside = PIPCheck(redTiles[i], redTiles[j], redTiles);
+      //     std::cerr << std::boolalpha << i << " " << j << " " << isInside <<
+      //     "\n";
+      if (A > maxRectangleEncased && isInside) {
+        maxRectangleEncased = A;
+      }
+    }
+  }
+  std::cout << "biggest area iside part2: " << maxRectangleEncased << "\n";
 }
